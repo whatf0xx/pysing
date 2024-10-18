c50bcbec0172ef8cd2e534b8d2489d460b75b846
whatf0xx <hfox022@gmail.com>
2024-10-18 12:26:24 +0200

    Coupling close to critical coupling, varying field, expected results

--- run.py
+++ run.py
@@ -5,24 +5,48 @@ from model import Model
 
 if __name__ == "__main__":
     m = Model(200,
-              field=-6.0e-6,
+              field=1.0e-3,
               coupling=5.4e-1)
-    s = 4
-    t = 5
-    fig, axs = plt.subplots(nrows=s, ncols=s, figsize=(9,9))
-    for i, j in product(range(s), range(s)):
-        m.plot_to_axes(axs[i][j])
-        axs[i][j].set_title(f"Step {(s*i+j) * t}")
-        # print(f"Beta={m.inverse_temp}")
-        # print(m.probability_gradient)
-        # print(m.evolution_probs)
-        for _ in range(t):
-            m.evolve()
+    n = Model(200,
+              field=1.0e-2,
+              coupling=5.4e-1)
+    p = Model(200,
+              field=1.0e-1,
+              coupling=5.4e-1)
 
-    # for _ in range(100):
-    #     m.evolve()
-    # m.plot_to_axes(axs[s-1][s-1])
-    # axs[s-1][s-1].set_title(f"Step {s**2 + 100}")
+    m1 = Model(200,
+              field=1.0e-3,
+              coupling=3.4e-1)
+    n1 = Model(200,
+              field=1.0e-2,
+              coupling=3.4e-1)
+    p1 = Model(200,
+              field=1.0e-1,
+              coupling=3.4e-1)
+
+
+    for _ in range(50):
+        m.evolve()
+        n.evolve()
+        p.evolve()
+        m1.evolve()
+        n1.evolve()
+        p1.evolve()
+
+    fig, ax = plt.subplots(2, 3)
+    m.plot_to_axes(ax[0][0])
+    n.plot_to_axes(ax[0][1])
+    p.plot_to_axes(ax[0][2])
+    m1.plot_to_axes(ax[1][0])
+    n1.plot_to_axes(ax[1][1])
+    p1.plot_to_axes(ax[1][2])
+
+    ax[0][0].set_title("H=1.0e-3; J=5.4e-1")
+    ax[0][1].set_title("H=1.0e-2; J=5.4e-1")
+    ax[0][2].set_title("H=1.0e-1; J=5.4e-1")
+    ax[1][0].set_title("H=1.0e-3; J=3.4e-1")
+    ax[1][1].set_title("H=1.0e-2; J=3.4e-1")
+    ax[1][2].set_title("H=1.0e-1; J=3.4e-1")
 
     plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
     fig.tight_layout()
