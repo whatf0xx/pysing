5ff0bb68e1ad3b40688ca1c1ccd13c71f99edf50
whatf0xx <hfox022@gmail.com>
2024-10-18 12:43:23 +0200

    Good demo of parameters with expected effects, next recast this in terms of T, not J

--- run.py
+++ run.py
@@ -4,49 +4,18 @@ from model import Model
 
 
 if __name__ == "__main__":
-    m = Model(200,
-              field=1.0e-3,
-              coupling=5.4e-1)
-    n = Model(200,
-              field=1.0e-2,
-              coupling=5.4e-1)
-    p = Model(200,
-              field=1.0e-1,
-              coupling=5.4e-1)
+    fig, axs = plt.subplots(3, 3)
+    for i, j in product(range(3), repeat=2):
+        h = float(f"1.0e-{3-i}")
+        k = float(f"{j+3}.8e-1")
+        m = Model(100,
+                  field=h,
+                  coupling=k)
+        for _ in range(50):
+            m.evolve()
 
-    m1 = Model(200,
-              field=1.0e-3,
-              coupling=3.4e-1)
-    n1 = Model(200,
-              field=1.0e-2,
-              coupling=3.4e-1)
-    p1 = Model(200,
-              field=1.0e-1,
-              coupling=3.4e-1)
-
-
-    for _ in range(50):
-        m.evolve()
-        n.evolve()
-        p.evolve()
-        m1.evolve()
-        n1.evolve()
-        p1.evolve()
-
-    fig, ax = plt.subplots(2, 3)
-    m.plot_to_axes(ax[0][0])
-    n.plot_to_axes(ax[0][1])
-    p.plot_to_axes(ax[0][2])
-    m1.plot_to_axes(ax[1][0])
-    n1.plot_to_axes(ax[1][1])
-    p1.plot_to_axes(ax[1][2])
-
-    ax[0][0].set_title("H=1.0e-3; J=5.4e-1")
-    ax[0][1].set_title("H=1.0e-2; J=5.4e-1")
-    ax[0][2].set_title("H=1.0e-1; J=5.4e-1")
-    ax[1][0].set_title("H=1.0e-3; J=3.4e-1")
-    ax[1][1].set_title("H=1.0e-2; J=3.4e-1")
-    ax[1][2].set_title("H=1.0e-1; J=3.4e-1")
+        m.plot_to_axes(axs[i][j])
+        axs[i][j].set_title(f"H={h}; J={k}")
 
     plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
     fig.tight_layout()
